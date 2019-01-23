bdc77e043b39e2ffa26cd0c39b01afa6c082dca5
Andy <byhook@163.com>
2019-01-23 16:04:31 +0800

    启动背景加入

--- android/app/src/main/java/com/onzhou/mikemusic/MainActivity.java
+++ android/app/src/main/java/com/onzhou/mikemusic/MainActivity.java
@@ -1,13 +1,30 @@
 package com.onzhou.mikemusic;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableStatusBar();
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    protected void enableStatusBar() {
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
 }
