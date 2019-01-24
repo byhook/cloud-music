f2a20ee58678b45a1870c1ba8d7e59e69f272885
Andy <byhook@163.com>
2019-01-24 11:55:40 +0800

    添加三个Tab页面

--- lib/home/HomePage.dart
+++ lib/home/HomePage.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: new AppBar(
-        title: new Text('主页面'),
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor: Color(0xFFCE3433),
+    return DefaultTabController(
+      length: 3,
+      child: new Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: new AppBar(
+          title: new Text('主页面'),
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Color(0xFFCE3433),
+          bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_bus)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ]
+          ),
+        ),
+        body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.directions_car, size: 128.0, color: Colors.grey),
+              Icon(Icons.directions_bus, size: 128.0, color: Colors.grey),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.grey),
+            ]
+        ),
       ),
-      body: null,
     );
   }
 }
